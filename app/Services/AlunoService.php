fa1a71f26d0beb4b784dd416fe00574d64f63e0c
felipe.messias <felipe.messias@salvador.ba.gov.br>
2026-02-27 16:50:55 -0300

    cadastro de aluno parciamente compleeto

--- app/Services/AlunoService.php
+++ app/Services/AlunoService.php
@@ -3,7 +3,10 @@
 namespace App\Services;
 
 use App\Models\Aluno;
+use App\Models\pessoa;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AlunoService
 {
@@ -28,5 +31,64 @@ class AlunoService
         }
 
         return $alunos;
+    }
+
+    public function store($idPessoaAluno, $idResp, $idTurma){
+        DB::beginTransaction();
+
+        try{
+            Aluno::create([
+                'id_resp_fin' => $idResp['resp_fin'],
+                'id_resp_pedag' => $idResp['resp_pedag'],
+                'id_pessoa' => $idPessoaAluno,
+                'id_turma' => $idTurma
+            ]);
+            DB::commit();
+            $msg = 'Aluno registrado com sucesso!';
+            return $msg;
+        }catch(Exception $e){
+
+            DB::rollback();
+            $msg = "Erro um tentar registrar: $e";
+
+            return $msg;
+        }
+    }
+
+    public function edit($id){
+        try{
+            $aluno = Aluno::join('pessoas as aluno_pessoa', 'alunos.id_pessoa', 'aluno_pessoa.id')
+                    ->join('pessoas as pedag_pessoa', 'alunos.id_resp_pedag', 'pedag_pessoa.id')
+                    ->join('pessoas as fin_pessoa', 'alunos.id_resp_fin', '=', 'fin_pessoa.id')
+                    ->join('turmas', 'alunos.id_turma', 'turmas.id')
+                    ->join('enderecos as aluno_end', 'aluno_pessoa.id_end', '=', 'aluno_end.id')
+                    ->leftJoin('enderecos as pedag_end', 'pedag_pessoa.id_end', '=', 'pedag_end.id')
+                    ->leftJoin('enderecos as fin_end', 'fin_pessoa.id_end', '=', 'fin_end.id')
+                    ->where('aluno_pessoa.id', $id)
+                    ->select(
+                                'aluno_pessoa.nome as aluno_nome',
+                                'pedag_pessoa.nome as responsavel_pedagogico',
+
+                                DB::raw("
+                                    CASE
+                                        WHEN alunos.id_resp_pedag != alunos.id_resp_fin
+                                        THEN fin_pessoa.nome
+                                        ELSE NULL
+                                    END as responsavel_financeiro
+                                "),
+
+                                'turmas.id'
+                    )->first();
+
+            return $aluno;
+
+        }catch(Exception $e){
+            $msg = "Erro ao consultado os dados: $e";
+            return $msg;
+        }
+    }
+
+    public function update(Request $req, $id){
+        
     }
 }
